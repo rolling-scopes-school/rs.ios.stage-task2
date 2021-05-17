a17cf98724e799b4615b94d6ade4500b666559ce
Dmitriy Konstantinov <dkonstantinov@i-neti.ru>
2021-05-17 20:08:35 +0300

    feat: added implementation for 1-4 exercise

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,36 @@
 #import "ReverseBinary.h"
 
+NSString* decToBinary(NSUInteger decInt)
+{
+    NSString *string = @"" ;
+    NSUInteger x = decInt ;
+    do {
+        string = [[NSString stringWithFormat: @"%lu", x&1] stringByAppendingString:string];
+    } while (x >>= 1);
+    return string;
+}
+
+NSString* reverseString(NSString* string)
+{
+    NSMutableString *reversedString = [NSMutableString stringWithCapacity:[string length]];
+
+    [string enumerateSubstringsInRange:NSMakeRange(0,[string length])
+                               options:(NSStringEnumerationReverse | NSStringEnumerationByComposedCharacterSequences)
+                            usingBlock:^(NSString *substring, NSRange substringRange, NSRange enclosingRange, BOOL *stop) {
+        [reversedString appendString:substring];
+    }];
+
+    for (int i = (int) 0; i < 8 - [string length]; i++)
+    {
+        [reversedString appendString:@"0"];
+    }
+
+    return reversedString;
+}
+
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+
+    NSString *string = decToBinary(n);
+    NSString *reversedString = reverseString(string);
+    return strtol([reversedString UTF8String], NULL, 2);
 }
--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,77 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+
+    NSMutableArray<NSNumber *> *numbersArray = [NSMutableArray arrayWithArray:@[]];
+    NSMutableArray<NSString *> *stringsArray = [NSMutableArray arrayWithArray:@[]];
+    NSMutableArray *resultArray = [NSMutableArray arrayWithArray:@[]];
+    BOOL isError = NO;
+
+    for (NSArray *insideArray in array) {
+        if (isError) { break; }
+        if (![insideArray isKindOfClass:[NSArray class]])
+        {
+            isError = YES;
+            break;
+        }
+
+        BOOL isKindOfNumber = NO;
+        BOOL isKindOfString = NO;
+
+        for (NSObject *element in insideArray) {
+
+            if ([element isKindOfClass:[NSNumber class]])
+            {
+                if (isKindOfString)
+                {
+                    isError = YES;
+                    break;
+                }
+                isKindOfNumber = YES;
+                [numbersArray addObject:(NSNumber*)element];
+            }
+            if ([element isKindOfClass:[NSString class]])
+            {
+                if (isKindOfNumber)
+                {
+                    isError = YES;
+                    break;
+                }
+                isKindOfString = YES;
+                [stringsArray addObject:(NSString*)element];
+            }
+        }
+    }
+
+    if (isError)
+    {
+        return resultArray;
+    }
+
+    if ([numbersArray count] > 0)
+    {
+        NSSortDescriptor *lowestToHighest = [NSSortDescriptor sortDescriptorWithKey:@"self" ascending:YES];
+        [numbersArray sortUsingDescriptors:[NSArray arrayWithObject:lowestToHighest]];
+        if ([stringsArray count] == 0) {
+            return numbersArray;
+        } else {
+            [resultArray addObject:numbersArray];
+        }
+    }
+
+    if ([stringsArray count] > 0)
+    {
+        BOOL ascending = [numbersArray count] > 0 ? NO : YES;
+        NSSortDescriptor *aToZ = [NSSortDescriptor sortDescriptorWithKey:@"self" ascending:ascending];
+        [stringsArray sortUsingDescriptors:[NSArray arrayWithObject:aToZ]];
+        if ([numbersArray count] == 0) {
+            return stringsArray;
+        } else {
+            [resultArray addObject:stringsArray];
+        }
+    }
+
+    return resultArray;
 }
 
 @end
